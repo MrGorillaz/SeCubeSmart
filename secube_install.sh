0839e42533a5a8b7618b7a237b5dffd0cc93851d
MrGorillaz <Alexander_Orlowski@gmx.de>
2025-04-07 17:27:44 +0200

    alternative uart

--- secube_install.sh
+++ secube_install.sh
@@ -5,4 +5,5 @@ sudo apt-get install git -y
 #Repo muss öffentlich sein
 git clone https://github.com/MrGorillaz/SeCubeSmart.git
 cd SeCubeSmart
-#ToDo
+#alternives UART Mapping
+echo "dtoverlay=uart4,txd4_pin=12,rxd4_pin=13" | sudo tee -a /boot/firmware/config.txt
