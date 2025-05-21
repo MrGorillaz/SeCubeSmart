a8c77968aa544b5e4415ffe41de4108f13912097
MrGorillaz <Alexander_Orlowski@gmx.de>
2025-05-21 21:07:31 +0200

    update

--- test_secube.py
+++ test_secube.py
@@ -2,7 +2,7 @@ import modules.secubeDriver as driver
 
 cube = driver.secubeDriver(debug=True,port='/dev/ttyAMA5')
 cube.get_version()
-
+''
 cube.get_status()
 #cube.set_led_level(60)
 cube.set_led_level(30)
